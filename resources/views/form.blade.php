032ace1f8eb0b07410b62ea8a2d393fb97a3d819
Unknown <kshayk0@gmail.com>
2018-05-13 11:01:06 +0300

    Added design to form

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,5 +1,7 @@
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
     <body>
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -7,17 +9,23 @@
         <form method="POST" action="/submit-form">
             @csrf
 
-            name: <input type="text" required>
-            <br>
-            Email: <input type="email" required>
-            <br>
-            subject: <input type="text" required>
-            <br>
-            content:
-            <br>
-            <textarea required></textarea>
-            <br>
-            <button type="submit">
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" name="name" id="name" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="subject">Subject:</label>
+                <input type="text" name="subject" id="subject" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="subject">Content:</label>
+                <textarea name="content" id="content" class="form-control" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-success">Submit</button>
         </form>
     </body>
 </html>
