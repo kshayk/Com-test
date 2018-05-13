ffd9328c7b546322708c2e9264e7654fd92693f4
Unknown <kshayk0@gmail.com>
2018-05-13 11:25:47 +0300

    Added csv logic

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -26,5 +26,31 @@ class ContactController extends Controller
     public function submitForm(Request $request)
     {
         print_r($request->all());
+
+        $random_string = $this->randomString(5);
+
+        //TODO: try catch this
+        $file = fopen("/var/tmp/csv_{$random_string}", "r");
+
+        $data_array = [
+            ['name', 'email', 'subject', 'content'],
+            [$request->input('name'), $request->input('email'), $request->input('subject'), $request->input('content')]
+        ];
+
+        //TODO: try catch this
+        fputcsv($file ,$data_array);
+
+        return response()->json('success', [], 200);
+    }
+
+    private function randomString($length = 5)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
